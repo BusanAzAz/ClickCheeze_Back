74a6f1c032d42f4bbb44d03eef7d848bc9af0a35
shk0625 <tjfgml1238@gmail.com>
2023-10-12 10:06:43 +0900

    🔧 :: setting.py recognize app adds

--- clickcheeze/settings.py
+++ clickcheeze/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "personal"
+    "personal",
+    "recognize"
 ]
 
 MIDDLEWARE = [
